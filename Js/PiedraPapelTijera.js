3abb215cc22b487138505300e4cf320088ac2668
jaimesandovalm <jaimesandovalm1990@gmail.com>
2023-05-29 13:32:39 -0400

    Juego Piedra-Papel-Tijera

---
+++ Js/PiedraPapelTijera.js
@@ -0,0 +1,15 @@
+//Declaracion de variables
+
+let eleccionDelJugador = 0
+eleccionDelJugador = prompt("Elije 1.-Piedra, 2.- Papel, 3.- Tijera")
+
+//informando la eleccion del jugador
+if (eleccionDelJugador == 1) {
+    alert("Elegiste Piedra")
+} else if(eleccionDelJugador == 2){
+    alert("Elegiste Papel")
+} else if (eleccionDelJugador == 3) {
+    alert("Elegiste Tijera")
+} else {
+    alert ("La eleccion elegida no es la correcta")
+}
